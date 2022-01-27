7a5cf627b916c3cc42ae80736ff0c7768f1efd78
AndreNavarro <andre-navarro@hotmail.com>
2022-01-26 21:51:44 -0300

    :sparkles: Adicionando State para username e navegaçao SPA

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-        }
-
-        body {
-            font-family: 'Open Sans', sans-serif;
-        }
-
-        /* App fit Height */
-        html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-        }
-        #__next{
-            flex: 1;
-        }
-        #__next > * {
-            flex: 1;
-        }
-        /* ./App fit Height*/
-    
-        `}</style>
-    );
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -69,11 +39,12 @@ export default HomePage
 */
 
 export default function PaginaInicial() {
-    const username = 'AndreNavarro';
+    // const username = 'AndreNavarro';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -100,6 +71,10 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (event) {
+                            event.preventDefault()
+                            roteamento.push('/chat')
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -109,8 +84,24 @@ export default function PaginaInicial() {
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
-
+                        {/*
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={function (event) {
+                                // Trocar o valor da variável
+                                const value = event.target.value
+                                setUsername(value);
+                            }}
+                        />
+                        */}
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                                // Troca o valor da variável
+                                const value = event.target.value
+                                setUsername(value);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
